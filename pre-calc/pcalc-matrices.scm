6ba419caa398df630b34ad4c87f9ac5b4b31d1f7
Nam Lah <nam-lah@users.noreply.github.com>
2018-07-23 20:31:08 -0600

    Update pcalc-matrices.scm

--- pre-calc/pcalc-matrices.scm
+++ pre-calc/pcalc-matrices.scm
@@ -1,15 +1,23 @@
-;; Recursive procedures for operating matrices.
-;; Ignoring "map". 
+;; Higher order recursions for rows in matrices.
+;; Ignoring "map", to have a fun practice.
 
-(define (dual-matrix-op term mat1 mat2)
+;; it takes a list (a row from a matrix)
+;; operates term thru the row term({ a₁  aₙ})
+;; outputs results in a new list.
+(define (row-op term mat)
+	(if (null? mat) ()
+	    (cons (term (car mat))
+	          (row-op term (cdr mat)))))
+
+
+;; It goes thru each element of two rows (lists)
+;; operates them; it outputs the results
+;; in a new row (list)
+(define (rows-op term mat1 mat2)
 	(if (null? (and mat1 mat2)) () 
 	    (cons (term (car mat1)
 	                (car mat2))
-		  (dual-matrix-op term
-				  (cdr mat1)
-				  (cdr mat2)))))
+		  (rows-op term
+			   (cdr mat1)
+			   (cdr mat2)))))
 						          
-(define (matrix-op term mat)
-	(if (null? mat) ()
-	    (cons (term (car mat))
-	          (matrix-op term (cdr mat)))))
